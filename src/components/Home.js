ecff69ea629eaa54f45bd8abdeb1855e865592d7
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-12 13:51:13 +0900

    Insert Router pages... home, texts, words

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Card>
+        <CardContent>Work Cloud App Consist Of React And Firebase</CardContent>
+      </Card>
+    );
+  }
+}
+
+export default Home;
