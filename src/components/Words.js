cd597bd61fe63b13ceaf5fc44e6d49105ffea422
4leaf.ysh <4leaf.ysh@gmail.com>
2020-04-15 17:55:46 +0900

    react textfile Upload & Move to Detail page

--- src/components/Words.js
+++ src/components/Words.js
@@ -12,7 +12,6 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
-import { BottomNavigation } from "@material-ui/core";
 
 const styles = (theme) => ({
   fab: {
